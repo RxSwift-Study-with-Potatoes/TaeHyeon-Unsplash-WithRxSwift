e59e6bd08b99a6d2ee9764835b03c27325797260
김태현 <taehyk528@gmail.com>
2021-10-18 23:44:33 +0900

    ✅[CHORE] : 시작점 코드 변경 (#1)

--- Unsplash-WithRxSwift/Unsplash-WithRxSwift/App/SceneDelegate.swift
+++ Unsplash-WithRxSwift/Unsplash-WithRxSwift/App/SceneDelegate.swift
@@ -14,11 +14,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        guard let mainViewController = UIStoryboard(name: "MainStoryboard", bundle: nil)
-                .instantiateViewController(withIdentifier: "MainViewController") as? MainViewController else { return }
-        
+
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UINavigationController(rootViewController: mainViewController)
+        window?.rootViewController = BaseTabBarController()
         window?.makeKeyAndVisible()
     }
 }
